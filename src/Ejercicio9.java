d7ee5c1f4229c6c0ee5ce2a38310cb884375c5e3
Smulli <78427713+Smulli@users.noreply.github.com>
2021-02-04 23:25:58 +0100

    Ejercicio 9 del UD5.

---
+++ src/Ejercicio9.java
@@ -0,0 +1,18 @@
+public class Ejercicio9 {
+	
+	public static void main (String args[]) {
+		
+		//Definimos el bucle e incrementamos la variable num.
+		for (int num = 1; num <= 100; num++) {
+			//Comprobamos si es divisible entre 2 y 3.
+			if(num%2==0 || num%3 == 0) {
+				System.out.println(num);
+			}
+			
+		}
+	}
+	
+}
+		
+		
+
